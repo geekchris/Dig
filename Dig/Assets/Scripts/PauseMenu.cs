55ba0b6352b970e2ac6ccba7069cb29ca4f71f7b
Christian Cuison <christian.cuison@outlook.com>
2019-10-16 21:14:09 -0400

    v0.152 Added Pause Functionality. Changed Charge meter to horizontal orientation

---
+++ Dig/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    public GameObject PauseMenuUI;
+    public void Quit()
+    {
+        Debug.Log("APPLICATION QUIT.");
+        Application.Quit();
+    }
+
+    /**public void Pause()
+    *{
+    *    if(isPaused)
+    *    {
+    *        Debug.Log("UNPAUSED");
+    *        Unpause();
+    *    }
+    *    else
+    *    {
+    *        Debug.Log("PAUSED");
+    *        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    *        Time.timeScale = 0.0f;
+    *    }
+    *}
+    */
+    public void Unpause()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        Debug.Log("UNPAUSED");
+        //StartCoroutine("PauseWait");
+    }
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        Debug.Log("PAUSED");
+        //StartCoroutine("PauseWait");
+    }
+
+    IEnumerator PauseWait()
+    {
+        yield return new WaitForSeconds(1.5f);
+    }
+}
